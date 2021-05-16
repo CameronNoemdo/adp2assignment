4309c7003b13566803430adbac1081e9ad7db13d
Cameron Noemdo <219115443@mycput.ac.za>
2021-05-16 16:10:14 +0200

    Added Collection collection interface to Student file

--- src/main/java/za/ac/cput/Student.java
+++ src/main/java/za/ac/cput/Student.java
@@ -14,5 +14,45 @@ public class Student
     {
         this.student = student;
     }
-    
+
+    public static Collection<String> collectionAdd()
+    {
+        Collection<String> studentNames=new LinkedList<>();
+        Student s1=new Student("John");
+        Student s2=new Student("Doe");
+        Student s3=new Student("Mary");
+        studentNames.add(s1.student);
+        studentNames.add(s2.student);
+        studentNames.add(s3.student);
+
+        return studentNames;
+    }
+
+    public static Collection<String> collectionRemove()
+    {
+        Collection<String> studentNames=new LinkedList<>();
+        Student s1=new Student("John");
+        Student s2=new Student("Doe");
+        Student s3=new Student("Mary");
+        studentNames.add(s1.student);
+        studentNames.add(s2.student);
+        studentNames.add(s3.student);
+
+        studentNames.remove("John");
+
+        return studentNames;
+    }
+
+    public static boolean collectionFind()
+    {
+        Collection<String> studentNames=new LinkedList<>();
+        Student s1=new Student("John");
+        Student s2=new Student("Doe");
+        Student s3=new Student("Mary");
+        studentNames.add(s1.student);
+        studentNames.add(s2.student);
+        studentNames.add(s3.student);
+
+        return studentNames.contains("John");
+    }
 }
