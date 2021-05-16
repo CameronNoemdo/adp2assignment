bb279f75e47fe304f8445fae4e7218f36b0597ae
Cameron Noemdo <219115443@mycput.ac.za>
2021-05-16 16:13:07 +0200

    Added test methods for Collection collection interfaces

--- src/test/java/za/ac/cput/StudentTest.java
+++ src/test/java/za/ac/cput/StudentTest.java
@@ -5,5 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StudentTest
 {
+    @Test
+    void testCollectionAdd()
+    {
+        int collectionSize=Student.collectionAdd().size();
+        assertEquals(3, collectionSize, "Failed to add students to the collection.");
+    }
 
+    @Test
+    void testCollectionRemove()
+    {
+        int collectionSize=Student.collectionRemove().size();
+        assertEquals(2, collectionSize, "Failed to remove student from collection.");
+    }
+
+    @Test
+    void testCollectionFind()
+    {
+        assertTrue(Student.collectionFind(),"Failed to find student in collection.");
+    }
 }
