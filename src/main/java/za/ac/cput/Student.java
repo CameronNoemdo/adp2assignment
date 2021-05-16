55c5eb29f5a89bd26106c1a75a0ea8a6104a27fe
Cameron Noemdo <219115443@mycput.ac.za>
2021-05-16 16:05:50 +0200

    Added Student constructors

--- src/main/java/za/ac/cput/Student.java
+++ src/main/java/za/ac/cput/Student.java
@@ -2,5 +2,15 @@ package za.ac.cput;
 
 public class Student
 {
+    public String student;
+
+    public Student() {
+
+    }
+
+    public Student(String student)
+    {
+        this.student = student;
+    }
     
 }
